06900d4adfbb38830b4a84b406e6e671d54afa16
hamzaberrada122-stack <hamza.berrada122@gmail.com>
2025-11-19 23:49:45 +0100

    done some more advanced visualisations using pandas

--- src/data/visualise_raw_data.py
+++ src/data/visualise_raw_data.py
@@ -9,27 +9,62 @@ CSV_PATH_GOOG = os.path.join(BASE_DIR, "data", "raw", "GOOG.csv")
 CSV_PATH_MSFT = os.path.join(BASE_DIR, "data", "raw", "MSFT.csv")
 CSV_PATH_NVDA = os.path.join(BASE_DIR, "data", "raw", "NVDA.csv")
 
-df_AMD=pd.read_csv(CSV_PATH_AMD)    
-df_GOOG=pd.read_csv(CSV_PATH_GOOG)  
-df_MSFT=pd.read_csv(CSV_PATH_MSFT)  
-df_NVDA=pd.read_csv(CSV_PATH_NVDA)  
+df_AMD=pd.read_csv(CSV_PATH_AMD,index_col='Date',parse_dates=True)    
+df_GOOG=pd.read_csv(CSV_PATH_GOOG,index_col='Date',parse_dates=True)  
+df_MSFT=pd.read_csv(CSV_PATH_MSFT,index_col='Date',parse_dates=True)  
+df_NVDA=pd.read_csv(CSV_PATH_NVDA,index_col='Date',parse_dates=True)  
 
 # line charts des valeurs des actions de chacune des compagnies
-x=df_AMD["Date"]
-y=df_AMD["Close"]
-plt.plot(x,y)
-plt.show()
-x=df_MSFT["Date"]
-y=df_MSFT["Close"]
-plt.plot(x,y)
-plt.show()
-x=df_GOOG["Date"]
-y=df_GOOG["Close"]
-plt.plot(x,y)
-plt.show()
-x=df_NVDA["Date"]
-y=df_NVDA["Close"]
-plt.plot(x,y)
+df_AMD['Close'].loc['2022'].plot()
+df_AMD['Close'].loc['2022'].resample('M').mean().plot(label='moyenne par mois',alpha=0.8,ls=':',lw=3)
+df_AMD['Close'].loc['2022'].resample('W').mean().plot(label='moyenne par semaine',alpha=0.8,ls='--',lw=2)
+
+plt.legend()
 plt.show()
 
-# 
+m= df_AMD['Close'].resample('M').agg(['mean','std','max','min'])
+
+m['mean'].plot(label='moyenne')
+plt.fill_between(m.index,m['min'],m['max'],alpha=0.2,label='min-max')
+plt.legend()
+plt.show()
+
+df_AMD['Close'].loc['2020'].rolling(window=3).mean().plot(label='moyenne en 3 jours')
+df_AMD['Close'].loc['2020'].rolling(window=10).mean().plot(label='moyenne en 10 jours',alpha=0.8,ls=':')
+df_AMD['Close'].loc['2020'].rolling(window=15).mean().plot(label='moyenne en 15 jours',alpha=0.6,ls='--')
+plt.legend()
+plt.show()
+
+for i in np.arange(0.2,1,0.2):
+    df_AMD['Close'].loc['2023-03'].ewm(alpha=i).mean().plot(label=f'ewm {i}',ls=':')
+
+plt.legend()
+plt.show()
+
+m2 = pd.merge(df_AMD,df_NVDA,on='Date',how='inner',suffixes=('_AMD','_NVIDIA'))
+
+m2[['Close_AMD','Close_NVIDIA']].plot()
+plt.show()
+
+m3 = pd.merge(df_GOOG,df_MSFT,on='Date',how='inner',suffixes=('_GOOGLE','_MICROSOFT'))
+
+m3[['Close_GOOGLE','Close_MICROSOFT']].plot(subplots=True)
+plt.show()
+
+# x=df_AMD["Date"]
+# y=df_AMD["Close"]
+# plt.plot(x,y)
+# plt.show()
+# x=df_MSFT["Date"]
+# y=df_MSFT["Close"]
+# plt.plot(x,y)
+# plt.show()
+# x=df_GOOG["Date"]
+# y=df_GOOG["Close"]
+# plt.plot(x,y)
+# plt.show()
+# x=df_NVDA["Date"]
+# y=df_NVDA["Close"]
+# plt.plot(x,y)
+# plt.show()
+
