7cb0d5e6a7b32a3fec011225750ea2f041dabb80
hamzaberrada122-stack <hamza.berrada122@gmail.com>
2025-11-19 15:59:07 +0100

    cleaned data and started working on visualtisation by creating visualise_raw_data.py

---
+++ src/data/visualise_raw_data.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
+import os 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CSV_PATH_AMD = os.path.join(BASE_DIR, "data", "raw", "AMD.csv")
+CSV_PATH_GOOG = os.path.join(BASE_DIR, "data", "raw", "GOOG.csv")
+CSV_PATH_MSFT = os.path.join(BASE_DIR, "data", "raw", "MSFT.csv")
+CSV_PATH_NVDA = os.path.join(BASE_DIR, "data", "raw", "NVDA.csv")
+
+df_AMD=pd.read_csv(CSV_PATH_AMD)    
+df_GOOG=pd.read_csv(CSV_PATH_GOOG)  
+df_MSFT=pd.read_csv(CSV_PATH_MSFT)  
+df_NVDA=pd.read_csv(CSV_PATH_NVDA)  
+
+print(df_AMD.columns)
+
+x=df_AMD["Date"]
+y=df_AMD["Close"]
+
+plt.plot(x,y)
+plt.show()
